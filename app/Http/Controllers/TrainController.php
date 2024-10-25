956f95dfcf6f924f021e48ab39c9fa3808a42167
jacopolegovini <legovini96@gmail.com>
2024-10-25 16:49:17 +0200

    filter created

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Train;
+use Illuminate\Support\Carbon;
 
 class TrainController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $now = Carbon::now();
+        $trains = Train::where('departure_date', "=", "2024-10-25")->get();
+        // $trains = Train::all();
         return view("pages.home", compact("trains"));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,15 @@
 @section("page-title", "Homepage")
 
 @section("main-content")
-<h1>
-    Homepage
-</h1>
+<main>
+    <h1>
+        Homepage
+    </h1>
+    <div class="train-general-list">
+        <div class="card">
+            @dump($trains["id"]);
+        </div>
+    </div>
+
+</main>
 @endsection
