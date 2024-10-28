5bc71c743a37c923dc1ec0fcd217fed507dc2741
jacopolegovini <legovini96@gmail.com>
2024-10-28 12:11:46 +0100

    table created

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -12,54 +12,8 @@ class PassengerController extends Controller
      */
     public function index()
     {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Passenger $passenger)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Passenger $passenger)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Passenger $passenger)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Passenger $passenger)
-    {
-        //
+        $passengers = Passenger::all();
+        return view("pages.passenger", compact("passenger"));
     }
 }
