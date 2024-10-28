1a0a1e718db152bfd2e8e37889bf643ebadef2b3
jacopolegovini <legovini96@gmail.com>
2024-10-28 12:19:36 +0100

    controller issue fixed

--- database/seeders/PassengersSeeder.php
+++ database/seeders/PassengersSeeder.php
@@ -4,14 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as faker;
 
 class PassengersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+        }
     }
 }
