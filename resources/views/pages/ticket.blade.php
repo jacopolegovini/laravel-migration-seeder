c7148b9df0e5cbc33d7472ca6d8cf44472b09e5d
jacopolegovini <legovini96@gmail.com>
2024-10-28 16:31:53 +0100

    table created

--- resources/views/pages/ticket.blade.php
+++ resources/views/pages/ticket.blade.php
@@ -5,11 +5,27 @@
 @section("main-content")
 <main class="container">
     <table>
+        @foreach ($tickets as $ticket)
         <tr>
-            @foreach ( as )
 
-            @endforeach
         </tr>
+        @endforeach
+        @foreach ($tickets as $ticket)
+        <tr>
+            <td>
+                {{$ticket->user_name}}
+            </td>
+            <td>
+                {{$ticket->train_number}}
+            </td>
+            <td>
+                {{$ticket->departure_date}}
+            </td>
+            <td>
+                {{$ticket->departure_time}}
+            </td>
+        </tr>
+        @endforeach
     </table>
 </main>
 @endsection
