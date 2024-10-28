0681db1f61d28ba18b416e535275ff972ca7698e
jacopolegovini <legovini96@gmail.com>
2024-10-28 18:14:36 +0100

    bonus completed

--- database/seeders/CheckerSeeder.php
+++ database/seeders/CheckerSeeder.php
@@ -5,14 +5,29 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Checker;
 
 class CheckerSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Checker::truncate();
+        $heading = true;
+        $input_file = fopen(base_path("database\checkers.csv"), "r");
+        while (($record = fgetcsv($input_file, 1000, ",")) !== FALSE) {
+            if (!$heading) {
+                $Checker = array(
+                    "first_name" => $record['0'],
+                    "last_name" => $record['1'],
+                    "code" => $record['2'],
+                );
+                Checker::create($Checker);
+            }
+            $heading = false;
+        }
+        fclose($input_file);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PassengersSeeder::class,
-            TicketSeeder::class
+            TicketSeeder::class,
+            CheckerSeeder::class
         ]);
     }
 }
